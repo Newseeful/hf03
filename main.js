c63eebd55543bc402f3146bfab10053e57625320
handypeter <43230439+handypeter@users.noreply.github.com>
2020-06-27 04:19:54 +0200

    feat: EventListeners

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
-let newHeadline = document.querySelector('h1'); 
-newHeadline.textContent = 'A galéria összesen ' + gallery.length + ' képet tartalmaz'; 
+let newHeadline = document.querySelector('h1');
+newHeadline.textContent = 'A galéria összesen ' + gallery.length + ' képet tartalmaz';
 
 function showPage() {
     let pageNum = document.getElementById('page').value - 1;
     let photoContent = document.querySelector('#gallery');
     let photos = '';
     for (let i = 0; i < 10; i++) {
-        photos += `<div><img src=${gallery[i+pageNum*10][0]} alt="Dog${i+pageNum*10}"><div>${gallery[i+pageNum*10][1]}</div></div>`;
-      console.log(i+pageNum*10);  
+        photos += `<div><img src=${gallery[i + pageNum * 10][0]} id="${i}" alt="Dog${i + pageNum * 10}"><div>${gallery[i + pageNum * 10][1]}</div></div>`;
     }
-   photoContent.innerHTML = photos;
+    photoContent.innerHTML = photos;
+
+    for (let i = 0; i < 10; i++) {
+        let targetElement = document.getElementById(i);
+        targetElement.addEventListener("click", function () { alert("TODO: Lightbox"); });
+    }
 }
